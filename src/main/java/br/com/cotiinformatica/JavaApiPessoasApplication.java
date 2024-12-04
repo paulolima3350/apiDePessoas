3e9ee23c9bb45ed39fe8cbb76acc4494b1dc4a80
Paulo Lima <paulo22tr22@gmail.com>
2024-12-04 14:16:38 -0300

    subindo o inicio do projeto do crud de cadastro de pessoas com mensageria

---
+++ src/main/java/br/com/cotiinformatica/JavaApiPessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiPessoasApplication.class, args);
+	}
+
+}
